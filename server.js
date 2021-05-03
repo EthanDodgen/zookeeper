c298f6b178c9b40e83d9f90c79a3f82c8add5f22
Fival <thefivalco@gmail.com>
2021-05-03 14:09:40 -0500

    post route validation

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+const path = require('path')
 const {
   animals
 } = require('./data/animals')
@@ -6,7 +8,9 @@ const app = express()
 
 //middleware
 // parse incoming string or array data
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({
+  extended: true
+}))
 // parse incoming JSON data
 app.use(express.json())
 
@@ -57,6 +61,34 @@ function findById(id, animalsArray) {
   return result
 }
 
+function createNewAnimal(body, animalsArray) {
+  const animal = body
+  animalsArray.push(animal)
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({
+      animals: animalsArray
+    }, null, 2)
+  )
+  return animal
+}
+
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false
+  }
+  return true
+}
+
 //get routes
 app.get('/api/animals', (req, res) => {
   let results = animals
@@ -78,8 +110,16 @@ app.get('/api/animals/:id', (req, res) => {
 
 //post routes
 app.post('/api/animals', (req, res) => {
-  console.log(req.body)
-  res.json(req.body)
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString()
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.')
+  } else {
+    const animal = createNewAnimal(req.body, animals)
+    res.json(animal)
+  }
 })
 
 app.listen(PORT, () => {
